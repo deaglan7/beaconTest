4b70d7bd5446716c63ebe84ed76d27271d8e60ae
hack <hack@ionas-macbook-air.local>
2015-11-08 09:04:40 +0000

    Added a swift file

---
+++ beaconTest/LocationService.swift
@@ -0,0 +1,20 @@
+//
+//  LocationServices.swift
+//  beaconTest
+//
+//  Created by hack on 08/11/2015.
+//  Copyright © 2015 Anu Technologies. All rights reserved.
+//
+
+import Foundation
+
+@objc class LocationService : NSObject {
+    func getLocation() -> String
+    {
+        //return "hello world"
+        
+        let a = 1 + 2
+        return "hello world"
+    }
+}
+
